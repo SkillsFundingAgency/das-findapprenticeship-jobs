c937182b59171dfe1695ba84591aac7c0bc6b9e3
Sarah Duncan <sarah.duncan@education.gov.uk>
2023-11-09 10:34:41 +0000

    FAI-970 add unit tests

---
+++ SFA.DAS.FindApprenticeship.Jobs/SFA.DAS.FindApprenticeship.Jobs.UnitTests/Application/Services/WhenGettingLiveVacancies.cs
@@ -0,0 +1,29 @@
+﻿using AutoFixture.NUnit3;
+using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.FindApprenticeship.Jobs.Application.Services;
+using SFA.DAS.FindApprenticeship.Jobs.Domain.Interfaces;
+using SFA.DAS.FindApprenticeship.Jobs.Infrastructure.Api.Requests;
+using SFA.DAS.FindApprenticeship.Jobs.Infrastructure.Api.Responses;
+using SFA.DAS.Testing.AutoFixture;
+
+namespace SFA.DAS.FindApprenticeship.Jobs.UnitTests.Application.Services;
+public class WhenGettingLiveVacancies
+{
+    [Test, MoqAutoData]
+    public async Task Then_The_Api_Is_Called_And_Live_Vacancies_Returned(
+        GetLiveVacanciesApiResponse response,
+        [Frozen] Mock<IApiClient> apiClient,
+        RecruitService service)
+    {
+        apiClient.Setup(x =>
+        x.Get<GetLiveVacanciesApiResponse>(
+            It.Is<GetLiveVacanciesRequest>(c => c.GetUrl.Contains($"livevacancies?pageSize={It.IsAny<int>()}&pageNo={It.IsAny<int>()}"))))
+            .ReturnsAsync(response);
+
+        var actual = await service.GetLiveVacancies(It.IsAny<int>(), It.IsAny<int>());
+
+        actual.Should().BeEquivalentTo(response);
+    }
+}
---
+++ SFA.DAS.FindApprenticeship.Jobs/SFA.DAS.FindApprenticeship.Jobs.UnitTests/Infrastructure/Api/WhenCallingGet.cs
@@ -0,0 +1,93 @@
+﻿using AutoFixture.NUnit3;
+using System.Net;
+using FluentAssertions;
+using Microsoft.Extensions.Options;
+using Moq;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using SFA.DAS.FindApprenticeship.Jobs.Domain.Configuration;
+using SFA.DAS.FindApprenticeship.Jobs.Domain.Interfaces;
+using SFA.DAS.FindApprenticeship.Jobs.Infrastructure;
+using SFA.DAS.Testing.AutoFixture;
+
+namespace SFA.DAS.FindApprenticeship.Jobs.UnitTests.Infrastructure.Api;
+public class WhenCallingGet
+{
+    [Test, MoqAutoData]
+    public async Task Then_The_Endpoint_Is_Called_With_Authentication_Header_And_Data_Returned(
+        List<string> testObject,
+        FindApprenticeshipJobsApiConfiguration config)
+    {
+        config.BaseUrl = $"https://test.local/{config.BaseUrl}/";
+        var configMock = new Mock<IOptions<FindApprenticeshipJobsApiConfiguration>>();
+        configMock.Setup(x => x.Value).Returns(config);
+        var getTestRequest = new GetTestRequest();
+
+        var response = new HttpResponseMessage()
+        {
+            Content = new StringContent(JsonConvert.SerializeObject(testObject)),
+            StatusCode = System.Net.HttpStatusCode.Accepted
+        };
+
+        var httpMessageHandler = MessageHandler.SetupMessageHandlerMock(response, config.BaseUrl + getTestRequest.GetUrl, config.Key, HttpMethod.Get);
+        var client = new HttpClient(httpMessageHandler.Object);
+        var apiClient = new ApiClient(client, configMock.Object);
+
+        var actual = await apiClient.Get<List<string>>(getTestRequest);
+
+        actual.Should().BeEquivalentTo(testObject);
+    }
+
+    [Test, AutoData]
+    public void Then_If_It_Is_Not_Successful_An_Exception_Is_Thrown(
+    FindApprenticeshipJobsApiConfiguration config)
+    {
+        config.BaseUrl = $"https://test.local/{config.BaseUrl}/";
+        var configMock = new Mock<IOptions<FindApprenticeshipJobsApiConfiguration>>();
+        configMock.Setup(x => x.Value).Returns(config);
+        var getTestRequest = new GetTestRequest();
+        var response = new HttpResponseMessage
+        {
+            Content = new StringContent(""),
+            StatusCode = HttpStatusCode.BadRequest
+        };
+
+        var httpMessageHandler = MessageHandler.SetupMessageHandlerMock(response, config.BaseUrl + getTestRequest.GetUrl, config.Key, HttpMethod.Get);
+        var client = new HttpClient(httpMessageHandler.Object);
+        var apiClient = new ApiClient(client, configMock.Object);
+
+        Assert.ThrowsAsync<HttpRequestException>(() => apiClient.Get<List<string>>(getTestRequest));
+    }
+
+    [Test, AutoData]
+    public async Task Then_If_It_Is_Not_Found_Default_Is_Returned(
+    FindApprenticeshipJobsApiConfiguration config)
+    {
+        config.BaseUrl = $"https://test.local/{config.BaseUrl}/";
+        var configMock = new Mock<IOptions<FindApprenticeshipJobsApiConfiguration>>();
+        configMock.Setup(x => x.Value).Returns(config);
+        var getTestRequest = new GetTestRequest();
+        var response = new HttpResponseMessage
+        {
+            Content = new StringContent(""),
+            StatusCode = HttpStatusCode.NotFound
+        };
+
+        var httpMessageHandler = MessageHandler.SetupMessageHandlerMock(response, config.BaseUrl + getTestRequest.GetUrl, config.Key, HttpMethod.Get);
+        var client = new HttpClient(httpMessageHandler.Object);
+        var apiClient = new ApiClient(client, configMock.Object);
+
+        var actual = await apiClient.Get<List<string>>(getTestRequest);
+
+        actual.Should().BeNull();
+    }
+
+    private class GetTestRequest : IGetApiRequest
+    {
+        public GetTestRequest()
+        {
+        }
+        public string GetUrl => $"test-url/get";
+    }
+
+}
