00a025a711c6de1975b821aaf33e54022666bbfe
Sarah Duncan <sarah.duncan@education.gov.uk>
2023-11-29 10:37:41 +0000

    FAI-981 update http endpoint

--- src/SFA.DAS.FindApprenticeship.Jobs/Endpoints/HandleVacancyUpdatedEventHttp.cs
+++ src/SFA.DAS.FindApprenticeship.Jobs/Endpoints/HandleVacancyUpdatedEventHttp.cs
@@ -2,10 +2,11 @@ using System;
 using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using SFA.DAS.FindApprenticeship.Jobs.Domain.Handlers;
 using SFA.DAS.FindApprenticeship.Jobs.Infrastructure.Events;
+using System.Net.Http;
+using System.Text.Json;
 
 namespace SFA.DAS.FindApprenticeship.Jobs.Endpoints
 {
@@ -19,9 +20,10 @@ namespace SFA.DAS.FindApprenticeship.Jobs.Endpoints
         }
 
         [FunctionName("HandleVacancyUpdatedEventHttp")]
-        public async Task Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req, VacancyUpdatedEvent message, ILogger log)
+        public async Task Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestMessage req, ILogger log)
         {
             log.LogInformation($"HandleVacancyUpdatedEvent HTTP trigger function executed at {DateTime.Now}");
+            var message = JsonSerializer.Deserialize<VacancyUpdatedEvent>(req.Content.ReadAsStream());
             await _vacancyUpdatedHandler.Handle(message);
             log.LogInformation($"HandleVacancyUpdatedEvent HTTP trigger function finished at {DateTime.Now}");
         }
