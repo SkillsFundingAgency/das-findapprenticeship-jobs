631dac746f868ac37a189f2bcd571d5fdd38779a
Balaji Jambulingam <balaji.j@hotmail.co.uk>
2024-11-01 12:31:37 +0000

    Minor Renaming of Recruit Service to FindApprenticeshipJobsService

--- src/SFA.DAS.FindApprenticeship.Jobs/Application/Handlers/VacancyClosingSoonHandler.cs
+++ src/SFA.DAS.FindApprenticeship.Jobs/Application/Handlers/VacancyClosingSoonHandler.cs
@@ -1,33 +1,23 @@
 using SFA.DAS.FindApprenticeship.Jobs.Domain.Handlers;
 using SFA.DAS.FindApprenticeship.Jobs.Domain.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace SFA.DAS.FindApprenticeship.Jobs.Application.Handlers;
 
-public class VacancyClosingSoonHandler : IVacancyClosingSoonHandler
+public class VacancyClosingSoonHandler(IFindApprenticeshipJobsService findApprenticeshipJobsService, IDateTimeService dateTimeService)
+    : IVacancyClosingSoonHandler
 {
-    private readonly IFindApprenticeshipJobsService _recruitService;
-    private readonly IDateTimeService _dateTimeService;
     private const int PageSize = 500;
 
-    public VacancyClosingSoonHandler(IFindApprenticeshipJobsService recruitService, IDateTimeService dateTimeService)
-    {
-        _recruitService = recruitService;
-        _dateTimeService = dateTimeService;
-    }
     public async Task<IList<long>> Handle(int daysToExpire)
     {
-        var dateToUse = _dateTimeService.GetCurrentDateTime().AddDays(daysToExpire);
+        var dateToUse = dateTimeService.GetCurrentDateTime().AddDays(daysToExpire);
         
         var pageNo = 1;
         var totalPages = 100;
         var vacancyReferences = new List<long>();
         while (pageNo <= totalPages)
         {
-            var liveVacancies = await _recruitService.GetLiveVacancies(pageNo, PageSize, dateToUse);
+            var liveVacancies = await findApprenticeshipJobsService.GetLiveVacancies(pageNo, PageSize, dateToUse);
             
 
             totalPages = liveVacancies?.TotalPages ?? 0;
