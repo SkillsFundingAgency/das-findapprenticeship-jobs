da3e4bedad2895a7d5ed9a83eb179757708daec8
Balaji Jambulingam <balaji.j@hotmail.co.uk>
2024-12-17 09:34:04 +0000

    Typo corrected in the Unit test

--- src/SFA.DAS.FindApprenticeship.Jobs.UnitTests/Domain/WhenCastingApiResponseToAzureDocument.cs
+++ src/SFA.DAS.FindApprenticeship.Jobs.UnitTests/Domain/WhenCastingApiResponseToAzureDocument.cs
@@ -36,7 +36,7 @@ public class WhenCastingApiResponseToAzureDocument
             AssertWageIsMapped(apprenticeAzureSearchDocument, source);
             AssertCourseIsMapped(apprenticeAzureSearchDocument, source);
             AssertAddressIsMapped(apprenticeAzureSearchDocument, source);
-            AssetOtherAddressesAreMapped(apprenticeAzureSearchDocument, source);
+            AssertOtherAddressesAreMapped(apprenticeAzureSearchDocument, source);
             apprenticeAzureSearchDocument.IsPrimaryLocation.Should().Be(source.IsPrimaryLocation);
             apprenticeAzureSearchDocument.Location.Should().NotBeNull();
             apprenticeAzureSearchDocument.NumberOfPositions.Should().Be(source.NumberOfPositions);
@@ -99,7 +99,7 @@ public class WhenCastingApiResponseToAzureDocument
                 opt.Excluding(x => x.Weighting));
     }
 
-    private static void AssetOtherAddressesAreMapped(ApprenticeAzureSearchDocument apprenticeAzureSearchDocument, LiveVacancy source)
+    private static void AssertOtherAddressesAreMapped(ApprenticeAzureSearchDocument apprenticeAzureSearchDocument, LiveVacancy source)
     {
         apprenticeAzureSearchDocument.OtherAddresses.Should().BeEquivalentTo(source.OtherAddresses);
     }
