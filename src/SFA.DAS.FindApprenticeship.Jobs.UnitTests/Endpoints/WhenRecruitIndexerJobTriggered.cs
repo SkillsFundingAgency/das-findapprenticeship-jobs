1265500845fbf474a0948fac97d6058e9e70650c
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2025-04-29 22:26:21 +0100

    FAI-2509 - missing tests

--- src/SFA.DAS.FindApprenticeship.Jobs.UnitTests/Endpoints/WhenRecruitIndexerJobTriggered.cs
+++ src/SFA.DAS.FindApprenticeship.Jobs.UnitTests/Endpoints/WhenRecruitIndexerJobTriggered.cs
@@ -3,6 +3,7 @@ using Microsoft.Azure.Functions.Worker;
 using Moq;
 using NUnit.Framework;
 using SFA.DAS.FindApprenticeship.Jobs.Domain.Handlers;
+using SFA.DAS.FindApprenticeship.Jobs.Domain.Interfaces;
 using SFA.DAS.FindApprenticeship.Jobs.Endpoints;
 using SFA.DAS.Testing.AutoFixture;
 
@@ -12,10 +13,29 @@ public class WhenRecruitIndexerJobTriggered
     [Test, MoqAutoData]
     public async Task Then_The_Index_Is_Created(
         [Frozen] Mock<IRecruitIndexerJobHandler> handler,
+        [Frozen] Mock<IDateTimeService> dateTimeService,
         RecruitIndexerTimerTrigger sut)
     {
+        dateTimeService.Setup(x=>x.GetCurrentDateTime()).Returns(new DateTime(2020, 01, 01, 07,0,0));
+        
         await sut.Run(It.IsAny<TimerInfo>());
 
         handler.Verify(x => x.Handle(), Times.Once());
+    }
+    
+    [Test]
+    [MoqInlineAutoData(5)]
+    [MoqInlineAutoData(21)]
+    public async Task Then_The_Index_Is_Not_Created_When_Outside_Of_Time_Frame(
+        int hour,
+        [Frozen] Mock<IRecruitIndexerJobHandler> handler,
+        [Frozen] Mock<IDateTimeService> dateTimeService,
+        RecruitIndexerTimerTrigger sut)
+    {
+        dateTimeService.Setup(x=>x.GetCurrentDateTime()).Returns(new DateTime(2020, 01, 01, hour,0,0));
+        
+        await sut.Run(It.IsAny<TimerInfo>());
+
+        handler.Verify(x => x.Handle(), Times.Never());
     }
 }
--- src/SFA.DAS.FindApprenticeship.Jobs/Endpoints/IndexCleanupTimerTrigger.cs
+++ src/SFA.DAS.FindApprenticeship.Jobs/Endpoints/IndexCleanupTimerTrigger.cs
@@ -1,13 +1,14 @@
 using SFA.DAS.FindApprenticeship.Jobs.Domain.Handlers;
+using SFA.DAS.FindApprenticeship.Jobs.Domain.Interfaces;
 
 namespace SFA.DAS.FindApprenticeship.Jobs.Endpoints;
 
-public class IndexCleanupTimerTrigger(IIndexCleanupJobHandler handler, ILogger<IndexCleanupTimerTrigger> log)
+public class IndexCleanupTimerTrigger(IIndexCleanupJobHandler handler, ILogger<IndexCleanupTimerTrigger> log, IDateTimeService dateTimeService)
 {
     [Function("IndexCleanupTimerTrigger")]
     public async Task Run([TimerTrigger("0 */60 * * * *")] TimerInfo myTimer)
     {
-        if (DateTime.UtcNow.Hour >= 6 && DateTime.UtcNow.Hour <= 20)
+        if (dateTimeService.GetCurrentDateTime().Hour >= 6 && dateTimeService.GetCurrentDateTime().Hour <= 20)
         {
             log.LogInformation($"IndexCleanupTimerTrigger function executed at: {DateTime.UtcNow}");
             await handler.Handle();
